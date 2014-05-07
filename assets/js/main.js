4887c418169873ce097155c21b9911767a7755c2
Aleksandr Ivanov <aivanov@informatica.ru>
2014-05-07 12:31:55 +0400

    вынос в отдельный модуль загрузку файла

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+/**
+ * Created by aivanov on 06.05.2014.
+ */
+/*socket.request('/', {}, function (response) {
+ var t = response;
+ });*/
+
+var currentSessionId = 0;
+
+socket.on('prepReportCompleted', function (session) {
+    if (session.id == currentSessionId) {
+
+        nextStep(session.state);
+        // todo вывести предварительный отчет
+        var report = JSON.parse(session.prepReport);
+        document.getElementById("FsTotalQuantity").innerHTML = report.FsObjInDebtsData.totalFsObjects;
+        document.getElementById("FsUpdateQuantity").innerHTML = report.FsObjInDebtsData.updateQuantity;
+        document.getElementById("FsMissQuantity").innerHTML = report.FsObjInDebtsData.missQuantity;
+        document.getElementById("FsMissObjList").innerHTML = report.FsObjInDebtsData.missList;
+
+        document.getElementById("DebtsTotalQuantity").innerHTML = report.DebtsDataInFsObj.totalDebtsObjects;
+        document.getElementById("DebtsUpdateQuantity").innerHTML = report.DebtsDataInFsObj.updateQuantity;
+        document.getElementById("DebtsMissQuantity").innerHTML = report.DebtsDataInFsObj.missQuantity;
+        document.getElementById("DebtsMissObjList").innerHTML = report.DebtsDataInFsObj.missList;
+    }
+});
+
+function clearActiveClass() {
+    $('li').removeClass('active');
+}
+function nextStep(step) {
+    clearActiveClass();
+    $('#' + step).addClass('active');
+}
+
+
+function updateDebts () {
+    $.ajax({
+        url: "debt/debtsUpdate",
+        type: "POST",
+        data: {
+            currentSessionId: currentSessionId
+        },
+        dataType: "json",
+        success: function (res) {
+            var t = res;
+        }
+    });
+   /* $.post("debt/debtsUpdate",
+        {
+            currentSessionId: currentSessionId
+        },
+        function(data,status){
+            alert("Data: " + data + "\nStatus: " + status);
+        });*/
+}
---
+++ assets/js/uploadFile.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    function beforeSubmit() {
+        //check whether client browser fully supports all File API
+        if (window.File && window.FileReader && window.FileList && window.Blob) {
+            var fsize = $('#file')[0].files[0].size;
+            var ftype = $('#file')[0].files[0].type;
+            //allow file types
+            switch (ftype) {
+                case 'application/vnd.ms-excel':
+                    break;
+                default:
+                    $("#output").html("<div class='wrongMessage'>Попытка загрузить файл с неверным форматом!(<b>" + ftype + "</b>).  Необходимо загрузить файл в формате <b>*.xls</b></div>");
+                    return false
+            }
+
+            //Проверяем, если размер файла больше 100 MB (1048576 = 1 mb)
+            if (fsize > 104857600) {
+                $("#output").html("<div class='wrongMessage'>Размер файла превышает позволенный максимум (100МБ)!</div>");
+                return false
+            }
+        }
+        else {
+            alert("Пожалуйста, обновите свой браузер, так как у вашего браузера не хватает некоторых новых функций!");
+            return false
+        }
+    }
+    function OnProgress(event, position, total, percentComplete) {
+        //Progress bar
+        $('#progressbox').show();
+        $('#progressbar').width(percentComplete + '%'); //update progressbar percent complete
+        $('#statustxt').html(percentComplete + '%'); //update status text
+        if (percentComplete > 50) {
+            $('#statustxt').css('color', '#000'); //change status text to white after 50%
+        }
+    }
+    function afterSuccess (res) {
+        currentSessionId = res.sessionId;
+    }
+    var options = {
+        target: '#output',   // target element(s) to be updated with server response
+        beforeSubmit: beforeSubmit,  // pre-submit callback
+        uploadProgress: OnProgress, //upload progress callback
+        success: afterSuccess,
+        resetForm: false        // reset the form after successful submit
+    };
+
+    $('#form').submit(function (e) {
+        $(this).ajaxSubmit(options);
+        return false;
+    });
+});
