fe061efe75b4321a804df05572203513687cc187
Aleksandr Ivanov <aivanov@informatica.ru>
2014-05-07 12:39:47 +0400

    добавление в debtsController - debtsUpdate

--- api/controllers/DebtController.js
+++ api/controllers/DebtController.js
@@ -20,32 +20,49 @@ module.exports = {
         if (req.method === 'POST') {
             var filepath = req.files.file.path;
             Sessions.create({
-                filePath: filepath
+                filePath: filepath,
+                state: 'uploadFile'
             }).done(function (err, session){
                 if (err) {
                     return res.json({"error": err});
                 }else {
                     // Подготовка отчета.
                     var FsUrl = "http://si-sdiis/arcgis/rest/services/ai/test_shields/FeatureServer/0"; // сервис рекламные конструкции с задолженностями
-                    debts.buildSessionReport(session, FsUrl, function (err, result) {
+                    debts.buildPrepReport(session, FsUrl, function (err, result) {
                         if (err) {
-                            session.report = err;
+                            session.prepReport = err;
                             session.save();
                         } else {
-                            session.report = result;
+                            session.state = 'prepReport';
+                            session.prepReport = result;
+
                             session.save(function(err){
                                 if (err){
-                                    //todo вызов sessionCompleted с ошибкой
+                                    //todo вызов prepReportCompleted с ошибкой
                                 }
                             });
-                            //todo По окончании вызов sessionCompleted
+                            debts.messageEmit('prepReportCompleted', session);
                         }
                     });
-                    return res.json({session:session});
+                    return res.json({sessionId:session.id});
                 }
             });
         } else {
             res.view();
+        }
+    },
+
+    debtsUpdate: function (req, res) {
+        if (req.method === 'POST') {
+            Sessions.findOne({
+                id: req.body.currentSessionId
+            }).done(function(err, session) {
+                if (err) {
+                    return console.log(err);
+                } else {
+                    // todo начать обновление задолженности в слое
+                }
+            });
         }
     },
 
