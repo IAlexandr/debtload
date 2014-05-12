9b85197e135354f77a7d9054896a1c4a8cd2b5ac
Aleksandr Ivanov <aivanov@informatica.ru>
2014-05-12 16:19:50 +0400

    в debtController добавил reportToXls

--- api/controllers/DebtController.js
+++ api/controllers/DebtController.js
@@ -14,12 +14,14 @@
  *
  * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
+var path = require('path');
 
 module.exports = {
     prepare: function (req, res) {
         if (req.method === 'POST') {
             var filepath = req.files.file.path;
-            var FsUrl = "http://si-sdiis/arcgis/rest/services/ai/test_shields/FeatureServer/0"; // сервис рекламные конструкции с задолженностями в котором будет обновлена задолженность
+            // var FsUrl = "http://gis.informatica.ru/arcgis/rest/services/ai/test_shields/FeatureServer/0";
+           var FsUrl = "http://si-sdiis/arcgis/rest/services/ai/test_shields/FeatureServer/0"; // сервис рекламные конструкции с задолженностями в котором будет обновлена задолженность
             /**  создание сессии
              * возвращает {sessionId: session.id}, асинхронно запускает подготовку предварительного отчета,
              * который после выполнения вызовет sockets.emit(prepReportCompleted)
@@ -50,6 +52,19 @@ module.exports = {
         }
     },
 
+    reportToXls: function (req, res) {
+        debts.updateReportToXls(req.query.currentSessionId, function (err, result) {
+
+            if (err) {
+                res.json(err);
+            } else {
+                if (result.message = 'done.'){  // todo
+                    var pth = path.resolve(__dirname + '../../../tmp/updateReport' + result.sessionId+ '.xls');
+                    res.sendfile(pth);
+                }
+            }
+        });
+    },
     /**
      * Overrides for the settings in `config/controllers.js`
      * (specific to DebtController)
